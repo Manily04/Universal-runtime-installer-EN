ea5dbf379b641b2f779ffff8cc7c03027711f58c
Manily <77436704+Manily04@users.noreply.github.com>
2022-07-31 08:21:11 +0200

    Update Download-Script.ps1

--- Download-Script.ps1
+++ Download-Script.ps1
@@ -1,4 +1,5 @@
 @echo off
+color 07
 echo [%time% ] - log start >"%userprofile%\Downloads\Runtime installer Log.log"
 echo - >>"%userprofile%\Downloads\Runtime installer Log.log"
 mkdir C:\Users\Public\Downloads\VC >nul 2>&1
